bf69a4eb87a12701cfa5e17df0cb8dd326220743
Pravin Raj <128983635+pravinkr05@users.noreply.github.com>
2024-07-11 19:16:00 +0530

    Add files via upload

---
+++ Stream_Socket_Client_Server_Program/ssclient.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 5000
+#define BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[]) {
+    int client_socket, valread;
+    struct sockaddr_in server_address;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // create a TCP/IP socket
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // set server address and port
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    // convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // connect to server
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        perror("Connection error");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server.\n");
+
+    // loop for interactive communication
+    while (1) {
+        // send message to server
+        printf("Enter message: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        // receive response from server
+        valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread == 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Received message: %s\n", buffer);
+    }
+
+    close(client_socket);
+    return 0;
+}
